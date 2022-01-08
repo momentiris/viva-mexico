3de17b0c0fd942c106e0bd5cae843061b19360f5
Andreas Lundqvist <momentiris@gmail.com>
2022-01-08 16:01:38 +0100

    extract themehome for readability

--- src/ThemeHome.jsx
+++ src/ThemeHome.jsx
@@ -5,29 +5,32 @@ export const ThemeHome = ({ theme }) => {
     <div>
       <h1 className="heading-1">{theme.label}</h1>
       {theme.content.map((content, index) => (
-        <div key={index}>
-          <h2 className="text-xl font-bold">{content.introduction.heading}</h2>
-          <div>
-            {content.introduction.body.map((textElement) => (
-              <div key={textElement.key} className="mb-4">
-                {textElement}
-              </div>
-            ))}
-          </div>
-          <div>
-            {content.timeline.map((timelineInstance) => (
-              <div key={timelineInstance.slug} className="mb-2">
-                <Link
-                  to={timelineInstance.slug}
-                  className="text-lg font-semibold"
-                >
-                  {timelineInstance.heading}
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
+        <ThemeContent content={content} key={index} />
       ))}
+    </div>
+  );
+};
+
+const ThemeContent = ({ content }) => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold">{content.introduction.heading}</h2>
+      <div>
+        {content.introduction.body.map((textElement) => (
+          <div key={textElement.key} className="mb-4">
+            {textElement}
+          </div>
+        ))}
+      </div>
+      <div>
+        {content.timeline.map((timelineInstance) => (
+          <div key={timelineInstance.slug} className="mb-2">
+            <Link to={timelineInstance.slug} className="text-lg font-semibold">
+              {timelineInstance.heading}
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
