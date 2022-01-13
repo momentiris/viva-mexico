4576db13279fa2acb864e3a5233322f877bcc74a
Andreas Lundqvist <momentiris@gmail.com>
2022-01-13 21:03:01 +0100

    screen one with three models

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,23 @@ import { Theme } from './Theme';
 import { InDepth } from './InDepth';
 import { Home } from './Home';
 
+import { Suspense } from 'react';
+import { ScreenOne } from './ScreenOne';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path=":theme/*" element={<Theme />} />
-        <Route path="in-depth/:slug" element={<InDepth />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<div>Loading</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/screen-one" element={<ScreenOne />} />
+
+          <Route path="in-depth/:slug" element={<InDepth />} />
+          <Route path=":theme/*" element={<Theme />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
 export default App;
-
-// <Canvas style={{ background: 'black' }}>
-//   <ambientLight intensity={0.6} color="white" />
-//   <spotLight
-//     intensity={0.6}
-//     angle={0.1}
-//     penumbra={1}
-//     position={[10, 15, 10]}
-//     castShadow
-//   />
-//   <Stars
-//     radius={100}
-//     depth={50}
-//     count={5000}
-//     factor={4}
-//     saturation={0}
-//     fade
-//   />
-
-//   <OrbitControls />
-//   <Model />
-// </Canvas>
---
+++ src/ScreenOne.jsx
@@ -0,0 +1,49 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+import Model from './Model';
+
+export const ScreenOne = () => {
+  return (
+    <div className="w-full h-full grid grid-cols-1 lg:grid-cols-2 grid-rows-3 lg:grid-rows-2 bg-gray-800 gap-2">
+      <Canvas className="bg-gray-400">
+        <ambientLight intensity={0.6} color="white" />
+        <spotLight
+          intensity={0.6}
+          angle={0.1}
+          penumbra={1}
+          position={[10, 15, 10]}
+          castShadow
+        />
+
+        <OrbitControls autoRotate={true} autoRotateSpeed={1} />
+        <Model />
+      </Canvas>
+      <Canvas className="bg-gray-400">
+        <ambientLight intensity={0.6} color="white" />
+        <spotLight
+          intensity={0.6}
+          angle={0.1}
+          penumbra={1}
+          position={[10, 15, 10]}
+          castShadow
+        />
+
+        <OrbitControls autoRotate={true} autoRotateSpeed={1} />
+        <Model />
+      </Canvas>
+      <Canvas className="bg-gray-400 col-span-1 lg:col-span-2">
+        <ambientLight intensity={0.6} color="white" />
+        <spotLight
+          intensity={0.6}
+          angle={0.1}
+          penumbra={1}
+          position={[10, 15, 10]}
+          castShadow
+        />
+        <OrbitControls autoRotate={true} autoRotateSpeed={1} />
+        <Model />
+      </Canvas>
+    </div>
+  );
+};
