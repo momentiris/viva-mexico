4f829712dbf39dfe30aff077f52087e137ca1718
Andreas Lundqvist <momentiris@gmail.com>
2022-01-22 12:33:58 +0100

    refactor: replace react-spring with framer-motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
 import { Theme } from './Theme';
-import { InDepth } from './InDepth';
+
 import { Home } from './Home';
 
 import { ScreenOne } from './ScreenOne';
 import { ModelRouter } from './ModelRouter';
 import { ThemePost } from './ThemePost';
 import { ThemeObjectText } from './ThemeObjectText';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function App() {
   return (
@@ -18,20 +19,31 @@ function App() {
 }
 
 const Router = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/theme/:theme" element={<Theme />} />
-      <Route path="/theme/:theme/post/:post" element={<ThemePost />} />
-      <Route
-        path="/theme/:theme/object-text/:slug"
-        element={<ThemeObjectText />}
-      />
+  const location = useLocation();
 
-      <Route path="model/:model" element={<ModelRouter />} />
-      <Route path="/screen/one" element={<ScreenOne />} />
-      {/* <Route path="/in-depth/:slug" element={<InDepth />} /> */}
-    </Routes>
+  return (
+    <AnimatePresence exitBeforeEnter={true}>
+      <motion.div
+        className="w-full h-full"
+        key={location.pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/theme/:theme" element={<Theme />} />
+          <Route path="/theme/:theme/post/:post" element={<ThemePost />} />
+          <Route
+            path="/theme/:theme/object-text/:slug"
+            element={<ThemeObjectText />}
+          />
+
+          <Route path="/model/:model" element={<ModelRouter />} />
+          <Route path="/screen/one" element={<ScreenOne />} />
+        </Routes>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
--- src/ModelAnimationMount.tsx
+++ src/ModelAnimationMount.tsx
@@ -1,26 +1,18 @@
-import { useSpring, animated } from '@react-spring/three';
+import { motion } from 'framer-motion-3d';
 
 type ModelAnimationMountProps = {
   children: React.ReactNode;
 };
 
 export const ModelAnimationMount = ({ children }: ModelAnimationMountProps) => {
-  const { scale, rotation } = useSpring({
-    from: {
-      scale: [0, 0, 0],
-      rotation: [0, -7, 0],
-    },
-    to: {
-      scale: [1, 1, 1],
-      rotation: [0, 0, 0],
-    },
-    config: { mass: 40, tension: 350, friction: 300, precision: 0.00001 },
-  });
-
   return (
-    //@ts-ignore
-    <animated.group scale={scale} rotation={rotation}>
+    <motion.group
+      scale={0}
+      rotation={[0, -7, 0]}
+      animate={{ rotateY: 0, scale: 1 }}
+      transition={{ type: 'spring', duration: 6 }}
+    >
       {children}
-    </animated.group>
+    </motion.group>
   );
 };
