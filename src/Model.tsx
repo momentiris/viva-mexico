0c5d02bb0e314fd08de7cc8318cbb36bb745c254
Andreas Lundqvist <momentiris@gmail.com>
2022-01-30 23:24:45 +0100

    feat: add more models

--- src/Model.tsx
+++ src/Model.tsx
@@ -1,38 +1,20 @@
-import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
 import { PresentationalScene } from './PresentationalScene';
 
-import {
-  Olmek,
-  SunStoneSmall,
-  Maya,
-  Toltek,
-  SacrificalStone,
-  SacrificialCasket,
-  Xolo,
-} from './models/index';
-
-const models: Record<string, React.ReactNode> = {
-  olmek: <Olmek />,
-  maya: <Maya />,
-  toltek: <Toltek />,
-  sacrificialstone: <SacrificalStone />,
-  sunstonesmall: <SunStoneSmall />,
-  sacrificialcasket: <SacrificialCasket />,
-  xolo: <Xolo />,
-};
+import { models } from './models/index';
 
 export const Model = () => {
-  const { model } = useParams<'model'>();
-  console.log('hello');
-  if (!model || !models[model]) {
+  const params = useParams<'model'>();
+  const model = models.find((model) => model.slug === params.model);
+
+  if (!model) {
     return <Navigate to="/" />;
   }
 
   return (
     <div className="bg-gradient-to-bl from-gray-900 to-gray-600 w-full h-full">
-      <PresentationalScene>{models[model]}</PresentationalScene>;
+      <PresentationalScene>{model.element}</PresentationalScene>;
     </div>
   );
 };
--- src/models/index.tsx
+++ src/models/index.tsx
@@ -5,6 +5,13 @@ import { Toltek } from './Toltek';
 import { SunStoneSmall } from './SunstoneSmall';
 import { SacrificialCasket } from './SacrificialCasket';
 import { Xolo } from './Xolo';
+import { Chalchiutlicue } from './Chalchiutlicue';
+import { KnifeFigure } from './KnifeFigure';
+import { MaskIncense } from './MaskIncense';
+import { Mictlantecutli } from './Mictlantecutli';
+import { Quetzalcoatl } from './Quetzalcoatl';
+import { Skarva } from './Skarva';
+import { Xochiquetzal } from './Xochiquetzal';
 
 export {
   Maya,
@@ -14,6 +21,13 @@ export {
   SunStoneSmall,
   SacrificialCasket,
   Xolo,
+  Chalchiutlicue,
+  MaskIncense,
+  KnifeFigure,
+  Mictlantecutli,
+  Quetzalcoatl,
+  Skarva,
+  Xochiquetzal,
 };
 
 export const models = [
@@ -32,4 +46,19 @@ export const models = [
     element: <SacrificialCasket />,
   },
   { slug: 'xolo', label: 'Xolo', element: <Xolo /> },
+  {
+    slug: 'chalchiutlicue',
+    label: 'Chalchiutlicue',
+    element: <Chalchiutlicue />,
+  },
+  { slug: 'maskincense', label: 'Mask Incense', element: <MaskIncense /> },
+  { slug: 'knifefigure', label: 'Knife Figure', element: <KnifeFigure /> },
+  {
+    slug: 'mictlantecutli',
+    label: 'Mictlantecutli',
+    element: <Mictlantecutli />,
+  },
+  { slug: 'quetzalcoatl', label: 'Quetzalcoatl', element: <Quetzalcoatl /> },
+  { slug: 'skarva', label: 'Skarva', element: <Skarva /> },
+  { slug: 'xochiquetzal', label: 'Xochiquetzal', element: <Xochiquetzal /> },
 ];
