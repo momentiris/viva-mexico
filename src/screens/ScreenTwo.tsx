5cd83e88ea82ad5c561bd0da21124c22581698ad
Andreas Lundqvist <momentiris@gmail.com>
2022-02-02 17:17:21 +0100

    fix: use correct models on screen two

--- src/screens/ScreenTwo.tsx
+++ src/screens/ScreenTwo.tsx
@@ -1,12 +1,12 @@
 import { PresentationalScene } from '../PresentationalScene';
-import { Toltek, SunStoneSmall } from '../models/index';
+import { SunStoneSmall, SacrificialCasket } from '../models/index';
 
 export const ScreenTwo = () => {
   return (
     <div className="w-screen h-screen grid grid-cols-1 lg:grid-cols-2 bg-gray-300 gap-1">
       <div className="bg-gray-700">
         <PresentationalScene>
-          <Toltek />
+          <SacrificialCasket />
         </PresentationalScene>
       </div>
       <div className="bg-gray-700">
