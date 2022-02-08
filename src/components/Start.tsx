64768d224f1d3301146e819fa0ed770d264bdb83
Andreas Lundqvist <momentiris@gmail.com>
2022-02-08 08:55:17 +0100

    try again

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -4,7 +4,7 @@ import { BgStart } from '../assets/bg-start';
 
 export const Start = () => (
   <div className="w-full h-full bg-lightGrey p-4 flex flex-col items-center justify-center">
-    <div className="flex items-center justify-center w-full max-w-xs md:max-w-xl">
+    <div className="flex items-center justify-center w-full max-w-xs md:max-w-xl h-1/3 md:h-1/2">
       <BgStart />
     </div>
 
@@ -13,7 +13,7 @@ export const Start = () => (
       meets death.
     </p>
     <Link
-      className="py-2 px-6 border-4 bg-blue text-white rounded-xl border-darkGrey text-4xl font-bold"
+      className="py-1 px-4 border-4 bg-blue text-white rounded-xl border-darkGrey text-3xl font-bold"
       to="themes"
     >
       Explore!
