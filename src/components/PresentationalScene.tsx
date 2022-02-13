06af2e86bb153f6a138b52a145b11ca499b2c94a
Andreas Lundqvist <momentiris@gmail.com>
2022-02-13 09:32:34 +0100

    feat: reset models after interaction inactivity

--- src/components/PresentationalScene.tsx
+++ src/components/PresentationalScene.tsx
@@ -8,31 +8,52 @@ type ModelWrapperProps = {
   children: React.ReactNode;
 };
 
-export const PresentationalScene = ({ children }: ModelWrapperProps) => (
-  <div className="w-full h-full">
-    <React.Suspense fallback={<Spinner />}>
-      <Canvas className="w-full h-full">
-        <ambientLight intensity={0.6} color="white" />
-        <spotLight
-          intensity={0.6}
-          angle={0.1}
-          penumbra={1}
-          position={[10, 15, 10]}
-          castShadow
-        />
+export const PresentationalScene = ({ children }: ModelWrapperProps) => {
+  const timeoutRef = React.useRef<number>();
+  const nodeRef = React.useRef<any>(null);
 
-        <OrbitControls
-          autoRotate={true}
-          autoRotateSpeed={1}
-          maxDistance={6}
-          minDistance={0}
-          enablePan={false}
-        />
-        <Float speed={2} rotationIntensity={1} floatIntensity={2}>
-          <ModelAnimationMount>{children}</ModelAnimationMount>
-        </Float>
-      </Canvas>
-    </React.Suspense>
-    <Loader containerStyles={{ backgroundColor: 'rgb(17 24 39)' }} />
-  </div>
-);
+  const handleInteractionEnd = () => {
+    window.clearTimeout(timeoutRef.current);
+
+    timeoutRef.current = window.setTimeout(() => {
+      console.log('reset');
+      nodeRef.current.reset();
+    }, 1000 * 60 * 3);
+  };
+
+  React.useEffect(() => {
+    () => window.clearTimeout(timeoutRef.current);
+  }, []);
+
+  return (
+    <div className="w-full h-full">
+      <React.Suspense fallback={<Spinner />}>
+        <Canvas className="w-full h-full">
+          <ambientLight intensity={0.6} color="white" />
+          <spotLight
+            intensity={0.6}
+            angle={0.1}
+            penumbra={1}
+            position={[10, 15, 10]}
+            castShadow
+          />
+
+          <OrbitControls
+            ref={nodeRef}
+            autoRotate={true}
+            autoRotateSpeed={1}
+            maxDistance={6}
+            minDistance={0}
+            enablePan={false}
+            position={[0, 0, 0]}
+            onEnd={handleInteractionEnd}
+          />
+          <Float speed={2} rotationIntensity={1} floatIntensity={2}>
+            <ModelAnimationMount>{children}</ModelAnimationMount>
+          </Float>
+        </Canvas>
+      </React.Suspense>
+      <Loader containerStyles={{ backgroundColor: 'rgb(17 24 39)' }} />
+    </div>
+  );
+};
